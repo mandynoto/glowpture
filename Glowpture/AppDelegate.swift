1c1fd61995840fd0984c8b3287b2a24cbb63cb46
Mandy Noto <mandynoto@icloud.com>
2023-06-21 13:06:37 -0700

    Initial Commit

---
+++ Glowpture/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Glowpture
+//
+//  Created by Mandy Noto on 6/21/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Glowpture/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Glowpture
+//
+//  Created by Mandy Noto on 6/21/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
